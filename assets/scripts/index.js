b8d1bf959808d203a5843a4a111096293084450e
Felipe <felipenegrelleramos@gmail.com>
2022-03-11 11:03:28 -0300

    testing dynamic objects in js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,15 @@ episodio-* (for normal episodes)
 emails-* (for emails episodes)
 especial-natal (for the unique xmas special episode)
 
-and inside each episode obj we have: a 'title', a 'descricao' and a 'id'
+and inside each episode obj we have: a 'title', a 'descricao' and a 'id', and once i put 'extra' but its just in one obj that i don't remember wich
 
 */ 
 
-console.log(data["episodio-1"].title, data['episodio-1'].id, data['emails-1'].id)
+const image = document.querySelector('.player__img');
+const title = document.querySelector('.player__title');
+
+image.src = './assets/images/testeImage.png';
+image.style.borderRadius = '1rem';
+title.textContent = data[`episodio-${data['episodio-1'].id}`].title;   
+
+console.log(data["episodio-1"].title, data['episodio-1'].id, data['emails-1'].id);
